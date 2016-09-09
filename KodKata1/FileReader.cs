4d21924d54fad7c2f27a7cd219728670712b1f2b
ElSiipo <cbartelius@gmail.com>
2016-09-09 11:44:04 +0200

    Initial Commit - KodKata1

---
+++ KodKata1/FileReader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace KodKata1
+{
+    public class FileReader
+    {
+        private StreamReader _streamReader;
+
+        private List<String> _blablaDictionary;
+        public List<String> BlablaDictionary
+        {
+            get { return _blablaDictionary; }
+            set 
+            {
+                if (_blablaDictionary != value)
+                {
+                    _blablaDictionary = value;
+                } 
+            }
+        }
+
+        private string _inputWord;
+        public string InputWord
+        {
+            get { return _inputWord; }
+            set { _inputWord = value; }
+        }
+
+        private string _filePath;
+
+        public FileReader(string filepath)
+        {
+            _blablaDictionary = new List<string>();
+            this._filePath = filepath;
+        }
+
+        public void ReadFile()
+        {
+            _streamReader = new StreamReader(_filePath);
+            
+            string line;
+
+            while ((line = _streamReader.ReadLine()) != null)
+            {
+                _blablaDictionary.Add(line);
+            }
+
+        }
+
+        public bool IsWordExistingInList(string _inputWord)
+        {
+           return BlablaDictionary.Contains(_inputWord);
+        }
+
+        public void ExcludeWordsByCount()
+        {
+            BlablaDictionary = BlablaDictionary.Where(p => p.Length == 4).ToList();
+        }
+    }
+}
---
+++ KodKataUnitTest/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using KodKata1;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KodKataUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void CanReadFile()
+        {
+            //FileReader fr = new FileReader("");
+            //fr.ReadFile();
+
+            //if (fr.BlablaSwedish.Count > 0)
+            //{
+
+            //}
+
+            //Assert.AreEqual(true, canRead);
+        }
+
+
+        [TestMethod]
+        public void HasLines()
+        {
+            FileReader fr = new FileReader(@"test.txt");
+            fr.ReadFile();
+
+            bool _hasLines = false;
+
+            if (fr.BlablaDictionary.Count > 0)
+            {
+                _hasLines = true;
+            }
+
+            Assert.AreEqual(true, _hasLines);
+        }
+
+
+        [TestMethod]
+        public void VerifyLineCount ()
+        {
+            FileReader fr = new FileReader(@"test.txt");
+            fr.ReadFile();
+
+            Assert.AreEqual(true, fr.BlablaDictionary.Count == 5);
+        }
+
+
+        [TestMethod]
+        public void IsWordExisting()
+        {
+            FileReader fr = new FileReader(@"test.txt");
+            fr.ReadFile();
+
+            var isExisting = fr.IsWordExistingInList("apa");
+
+            Assert.AreEqual(true, isExisting);
+        }
+
+
+        [TestMethod]
+        public void ExcludeWordsByLength()
+        {
+            FileReader fr = new FileReader(@"swedish.txt");
+            fr.ReadFile();
+
+            fr.ExcludeWordsByCount();
+
+
+            Assert.AreEqual(true, fr.BlablaDictionary.Where(p => p.Length > fr.InputWord.Count()));
+        }
+    }
+}
